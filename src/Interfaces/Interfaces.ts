8ea958cf5f75aa976ce8c282cc46f40fb515fbc7
Spas Ermenov <spascho62@gmail.com>
2024-01-16 01:23:58 +0200

    remove eslint disable

--- src/Interfaces/Interfaces.ts
+++ src/Interfaces/Interfaces.ts
@@ -4,17 +4,19 @@ export interface IAlert {
   severity: AlertColor;
   message: string;
 }
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export interface IResponse<T = any> {
   data: T;
 }
+
 export interface ILogin {
-        token: null | string;
+  token: null | string;
   message: string;
 }
+
 export interface IRegister {
   registered: boolean;
-message: string;
+  message: string;
 }
 
 export interface IExercise {
