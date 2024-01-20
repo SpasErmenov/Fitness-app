5b6a2603ccbe37bfe4e5c7217743908cd61e829e
Spas Ermenov <spascho62@gmail.com>
2024-01-20 14:09:18 +0200

    fix css for ImageBlock

--- src/components/ImageBlock/ImageBlock.tsx
+++ src/components/ImageBlock/ImageBlock.tsx
@@ -2,12 +2,12 @@ import styles from "./ImageBlock.module.css";
 
 export const ImageBlock = () => {
   return (
-    <div className={styles.imageBlock}>
-      <div className={styles.headers}>
+    <div className={styles.ImageBlock}>
+      <div className={styles.ImageBlock__Title}>
         <h1>Track more.</h1>
         <h1>Lift more.</h1>
       </div>
-      <p>
+      <p className={styles.ImageBlock__Content}>
         My fitness IDP for tracking your fitness progres. Please register and
         give it a try. I hope you like it.
       </p>
