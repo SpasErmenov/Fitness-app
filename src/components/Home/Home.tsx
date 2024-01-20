f5f4cc1fa17e2ec7a97db0b89839fb1fbde6fdf2
Spas Ermenov <spascho62@gmail.com>
2024-01-20 14:43:43 +0200

    fix css for Home and use path as variable

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,17 +2,19 @@ import { Button } from "@mui/material";
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
+import { CREATE_WORKOUT_PAGE } from "@/routes/paths.constants";
 
 export const Home = () => {
   const navigate = useNavigate();
-  const onStartNewWorkoutButtonClick = useCallback(() => {
-    navigate("/home/create-workout");
+
+  const onClick = useCallback(() => {
+    navigate(CREATE_WORKOUT_PAGE);
   }, [navigate]);
 
   return (
-    <div className={styles.homeBackGround}>
-      <div className={styles.startNewWorkoutButton}>
-        <Button variant="contained" onClick={onStartNewWorkoutButtonClick}>
+    <div className={styles.Home}>
+      <div className={styles.Home__Button}>
+        <Button variant="contained" onClick={onClick}>
           Start new workout
         </Button>
       </div>
