329c1d2f3a46ad9a67319a283e8b38581c6720a1
Spas Ermenov <spascho62@gmail.com>
2024-01-20 13:54:18 +0200

    fix css for FeedbackBlock

--- src/components/FeedbackBlock/FeedbackBlock.tsx
+++ src/components/FeedbackBlock/FeedbackBlock.tsx
@@ -4,15 +4,15 @@ import { FeedbackCard } from "@/components/UI/FeedbackCard/FeedbackCard";
 
 export const FeedbackBlock = () => {
   return (
-    <div className={styles.feedbackBlock}>
-      <div className={styles.headers}>
+    <div className={styles.FeedbackBlock}>
+      <div className={styles.FeedbackBlock__Title}>
         <h1>Our Fitness App for everyone.</h1>
         <h2>
           No matter your level of experience and your training program. <br />
           Use our application to achieve your goals.
         </h2>
       </div>
-      <div className={styles.grid}>
+      <div className={styles.FeedbackBlock__Grid}>
         {usersFeedbackBlockPicture.map((user) => (
           <FeedbackCard key={user.id} name={user.name} imgPath={user.imgPath} />
         ))}
